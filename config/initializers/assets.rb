20822fc6ac213890e3f8424fe50667fd363812eb
root <root@localhost.localdomain>
2014-07-18 21:40:42 -0500

    couple of forgotten files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,11 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( pen.css )
+Rails.application.config.assets.precompile += %w( pen.js )
+Rails.application.config.assets.precompile += %w( pen.helper.js )
+Rails.application.config.assets.precompile += %w( pen.options.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
